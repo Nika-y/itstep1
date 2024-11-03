b2a7cacd1053500eab789f121e64ac26bcd9f78e
Asus <veronika777yakovenko@gnail.com>
2024-11-03 20:23:34 +0200

    first commit

---
+++ Lesson 4.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self):
+        self.height = 170
+        height = 160
+
+        def printer(self):
+            print(self.height)
+            
+            nick = Student()
+            nick.printer()
+
+    def printer(self):
+        pass
