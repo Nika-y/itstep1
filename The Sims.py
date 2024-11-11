8831f4e7bd9c7955466c522bce0a30661353bf13
Asus <veronika777yakovenko@gnail.com>
2024-11-11 19:04:46 +0200

    first commit

--- The Sims.py
+++ The Sims.py
@@ -1,6 +1,6 @@
 import random
 class Human:
-    def __init__(self, name="None", job=None, home=None, car=None ):
+    def __init__(self, name="None", job=None, home=None, car=None, personality_description=None):
         self.name = name
         self.money = 100
         self.gladness = 50
@@ -8,6 +8,7 @@ class Human:
         self.job = job
         self.home = home
         self.car = car
+        self.personality_description = personality_description
 
     def get_job(self):
         if self.car.drive():
@@ -18,8 +19,12 @@ class Human:
         self.job = Job(job_list)
     def get_home(self):
         self.home = House()
+
     def get_car(self):
         self.car = Auto(brands_of_car)
+
+    def get_personality_description(self):
+            self.personality_description = PersonalityDescription(personality_list)
 
 class House:
     def __init__(self):
@@ -48,6 +53,11 @@ class Job:
         self.job = random.choice(list(job_list))
         self.selary = job_list[self.job]["gladness_less"]
 
+class PersonalityDescription:
+     def __init__(self, personality_list):
+        self.description = random.choice(list(personality_list))
+
+
 brands_of_car = {"Mclaren":{"fuel":100, "strength":100,"consumption": 6},
 "Mercedes":{"fuel":50, "strength":40,"consumption": 10},
 "Porshe":{"fuel":70, "strength":150,"consumption": 8},
@@ -57,3 +67,11 @@ job_list = {"Java developer":{"salary":50, "gladness_less": 10 },
 "Python developer":{"salary":40, "gladness_less": 3 },
 "C++ developer":{"salary":45, "gladness_less": 25 },
 "Rust developer":{"salary":70, "gladness_less": 1 },}
+
+personality_list = [
+    "Introvert",
+    "Extrovert",
+    "Ambivert",
+    "Analytical",
+    "Creative",
+    "Adventurous"]
