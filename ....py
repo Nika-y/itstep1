be8c60a316c9f472f31287251ffb9627e7a91f47
Asus <veronika777yakovenko@gnail.com>
2024-11-24 19:08:49 +0200

    first commit

---
+++ ....py
@@ -0,0 +1,31 @@
+result = []
+
+def divider(a, b):
+    if a < b:
+        raise ValueError("a менше за b")
+    if b > 100:
+        raise IndexError("b більше 100")
+    return a / b
+
+data = {
+    10: 2,
+    2: 5,
+    "123": 4,
+    18: 0,
+    8: 4
+}
+
+for key, value in data.items():
+    try:
+        res = divider(int(key), value)
+        result.append(res)
+    except ZeroDivisionError:
+        print(f"Помилка: Ділення на нуль (a={key}, b={value})")
+    except ValueError as e:
+        print(f"Помилка: {e} (a={key}, b={value})")
+    except IndexError as e:
+        print(f"Помилка: {e} (a={key}, b={value})")
+    except TypeError as e:
+        print(f"Помилка: Неправильний тип даних (a={key}, b={value}) - {e}")
+
+print("Результат:", result)
